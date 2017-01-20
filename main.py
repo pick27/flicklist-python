82ea66d8cb43c209b685fd7a17c7480ecc2cade6
Scott Land <101.scott@gmail.com>
2017-01-19 20:34:53 -0600

    display today and tommorow movies

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movies = []
+        movies.append("The Big Lebowski")
+        movies.append("Star Wars")
+        movies.append("Starman")
+        movies.append("The Notebook")
+        movies.append("The Day the Earth Stood Still")
+        movies.append("Star Trek: The Movie")
 
         # TODO: randomly choose one of the movies, and return it
+        movie_choice = random.choice(movies)
 
-        return "The Big Lebowski"
+        return movie_choice
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,7 +29,12 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
-
+        content += "<h1>Tommorow's Movie</h1>"
+        movie2 = self.getRandomMovie()
+        movie2 = movie
+        while movie == movie2:
+            movie2 = self.getRandomMovie()
+        content += "<p>" + movie2 + "</p>"
         self.response.write(content)
 
 app = webapp2.WSGIApplication([
